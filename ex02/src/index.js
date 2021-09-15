8d4db795101f7814c573eb78c0c2eeeffcba110f
bozo1999 <bozo.paragon@gmail.com>
2021-09-15 15:09:33 +0200

    Dodane više izmjena u folderima

---
+++ ex02/src/index.js
@@ -0,0 +1,14 @@
+function main() {
+    //Only change code below this line
+    //This is a single line Javascript comment
+    /*  document.write("This is multi-line comment!");
+        document.write("This is multi-line comment!");
+        document.write("This is multi-line comment!");
+        document.write("This is multi-line comment!");
+    */
+    //Only change code above this line
+}
+
+console.log(main());
+
+module.exports = main;
